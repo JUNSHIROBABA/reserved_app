9e7e379d1777c5a7e5df0f43c2cf381e75011ff1
JUNSHIROBABA <baba.junshiro@di-v.co.jp>
2021-01-11 19:50:13 +0900

    Revert "Merge pull request #2 from JUNSHIROBABA/register_user_without_CSS"
    This reverts commit 511718be440cb601ffb0f1429237bd5fae2629b7, reversing
    changes made to 898746897eb55215e0835bbc1a6f957f258dfcb9.

--- app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :prefecture
-end
