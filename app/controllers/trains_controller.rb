0d284fbc81b97c4fed76cea905c0c836e716e312
JUNSHIROBABA <baba.junshiro@di-v.co.jp>
2021-01-17 11:16:04 +0900

    register_train1

--- app/controllers/trains_controller.rb
+++ app/controllers/trains_controller.rb
@@ -4,8 +4,18 @@ class TrainsController < ApplicationController
   end
 
   def new
+    @train = Train.new
   end
 
   def create
+    binding.pry
+    Train.create(train_params)
   end
+
+  private
+
+  def train_params
+    params.require(:train).permit(:name, :departure_date, :departure_time)
+  end
+
 end
